9c053ed6353ddcb1d3876b7cce4642c83ae5152d
oscar-foxtrot <93640558+oscar-foxtrot@users.noreply.github.com>
2023-02-09 19:52:10 +0300

    Create problem2.py

---
+++ Unit 2 Simple Programs/Problem Set 2/problem2.py
@@ -0,0 +1,12 @@
+fmp = 10
+while(True):
+    curbalance = balance
+    for i in range(1, 13):
+        curbalance -= fmp
+        curbalance *= (1 +  annualInterestRate / 12)
+    if curbalance < 0:
+        break
+    else:
+        fmp += 10
+        
+print(fmp)
