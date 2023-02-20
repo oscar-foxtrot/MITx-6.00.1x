769c5e27292a567149c41ec04c29484e2f39485b
oscar-foxtrot <93640558+oscar-foxtrot@users.noreply.github.com>
2023-02-20 17:22:49 +0300

    Create problem3.py

---
+++ Unit 3 Structured Types/Problem Set 3/problem3.py
@@ -0,0 +1,11 @@
+def getAvailableLetters(lettersGuessed):
+    '''
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''
+    res = ''
+    for c in 'abcdefghijklmnopqrstuvwxyz':
+        if c not in lettersGuessed:
+            res += c
+    return res
