2830f7babf6f6ad02c58db3674bd4a2ed0b82f49
oscar-foxtrot <93640558+oscar-foxtrot@users.noreply.github.com>
2023-02-20 17:05:54 +0300

    Create problem1.py

---
+++ Unit 3 Structured Types/Problem Set 3/problem1.py
@@ -0,0 +1,11 @@
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    for c in secretWord:
+        if c not in lettersGuessed:
+            return False
+    return True
