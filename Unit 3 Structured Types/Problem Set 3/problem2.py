d75181ac6682529dbbdccbc1bb8d7d5e11a758ba
oscar-foxtrot <93640558+oscar-foxtrot@users.noreply.github.com>
2023-02-20 17:14:16 +0300

    Create problem2.py

---
+++ Unit 3 Structured Types/Problem Set 3/problem2.py
@@ -0,0 +1,14 @@
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    res = ''
+    for c in secretWord:
+        if c not in lettersGuessed:
+            res += '_ '
+        else:
+            res += c
+    return res
