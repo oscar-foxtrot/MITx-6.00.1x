57505a8ebb86ee620a3e71241f15d3741c3c7548
oscar-foxtrot <93640558+oscar-foxtrot@users.noreply.github.com>
2023-02-24 19:53:31 +0300

    Update integer_division.py

--- Unit 4 Good Programming Practices/Exercises/integer_division.py
+++ Unit 4 Good Programming Practices/Exercises/integer_division.py
@@ -2,6 +2,7 @@ def integerDivision(x, a):
     """
     x: a non-negative integer argument
     a: a positive integer argument
+    
     returns: integer, the integer division of x divided by a.
     """
     count = 0
