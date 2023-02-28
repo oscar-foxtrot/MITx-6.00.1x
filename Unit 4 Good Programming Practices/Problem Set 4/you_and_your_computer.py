dde9f970d9c7542918073c1ef0272d53cde41f3b
oscar-foxtrot <93640558+oscar-foxtrot@users.noreply.github.com>
2023-02-28 15:13:42 +0300

    Create you_and_your_computer.py

---
+++ Unit 4 Good Programming Practices/Problem Set 4/you_and_your_computer.py
@@ -0,0 +1,61 @@
+def playGame(wordList):
+    """
+    Allow the user to play an arbitrary number of hands.
+ 
+    1) Asks the user to input 'n' or 'r' or 'e'.
+        * If the user inputs 'e', immediately exit the game.
+        * If the user inputs anything that's not 'n', 'r', or 'e', keep asking them again.
+
+    2) Asks the user to input a 'u' or a 'c'.
+        * If the user inputs anything that's not 'c' or 'u', keep asking them again.
+
+    3) Switch functionality based on the above choices:
+        * If the user inputted 'n', play a new (random) hand.
+        * Else, if the user inputted 'r', play the last hand again.
+      
+        * If the user inputted 'u', let the user play the game
+          with the selected hand, using playHand.
+        * If the user inputted 'c', let the computer play the 
+          game with the selected hand, using compPlayHand.
+
+    4) After the computer or user has played the hand, repeat from step 1
+
+    wordList: list (string)
+    """
+    curhand = {}
+    begun = False
+    while True:
+        opt = input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ')
+        if opt == 'r' and not begun: 
+            print('You have not played a hand yet. Please play a new hand first!\n')
+            continue
+        elif opt not in ['n', 'r', 'e']:
+            print('Invalid command.\n')
+            continue
+        elif opt == 'e':
+            break
+          
+        print()
+        while True:
+            player = input('Enter u to have yourself play, c to have the computer play: ')
+            if player not in ['c', 'u']:
+                print('Invalid command.\n')
+            else:
+                break
+        
+        if opt == 'r':
+            if player == 'c':
+                compPlayHand(curhand, wordList, HAND_SIZE)
+            else:
+                playHand(curhand, wordList, HAND_SIZE)
+            print()
+        elif opt == 'n':
+            begun = True
+            curhand = dealHand(HAND_SIZE)
+            if player == 'c':
+                compPlayHand(curhand, wordList, HAND_SIZE)
+            else:
+                playHand(curhand, wordList, HAND_SIZE)
+            print()
+            
+    return
